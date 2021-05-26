b6d8edbd2e13027d9b50af716a5ea03bc50cf4a6
Nasifah <83665329+Nasifah@users.noreply.github.com>
2021-05-26 05:43:16 +0100

    first commit

---
+++ assets or statics/js/bar.js
@@ -0,0 +1,8 @@
+//to get access to bar from html
+const navItemContainer = document.getElementById("bar");
+
+
+//function to open and close bar
+function toggleNavbar(){
+    navItemContainer.classList.toggle("open-bar")
+}
